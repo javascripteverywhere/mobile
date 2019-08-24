2f5303d73d2e25dd189f00345a2f6998507b1eb8
Adam Scott <ascott1@gmail.com>
2019-08-24 16:29:59 -0400

    initial 'Hello World' content

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,12 @@
-// import React from 'react';
-// import { Text, View } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
 
-// const Main = () => {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text>Hello world!</Text>
-//     </View>
-//   );
-// };
+const Main = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello world!</Text>
+    </View>
+  );
+};
 
-// export default Main;
+export default Main;
