258e2ec9e35ba53ab1c13238ad4dbd97cd2e4af6
Adam Scott <ascott1@gmail.com>
2019-08-29 16:54:29 -0400

    add styling

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mobile
 [WIP] React Native code examples for JavaScript Everywhere by Adam Scott, published by O'Reilly Media 
+
+"Hello World" photo by [Windell Oskay](https://www.flickr.com/photos/oskay/472097903).
--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import styled from 'styled-components/native';
+
+const StyledView = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
+
+const H1 = styled.Text`
+  font-size: 48px;
+  font-weight: bold;
+`;
+
+const P = styled.Text`
+  margin: 24px 0;
+  font-size: 18px;
+`;
 
 const Main = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello world!</Text>
-    </View>
+    <StyledView>
+      <H1>Hello world!</H1>
+      <P>This is my app, isn't it wonderful?</P>
+      <Image source={require('../assets/images/hello-world.jpg')} />
+    </StyledView>
   );
 };
 
