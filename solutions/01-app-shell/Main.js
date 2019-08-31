fe60503ed57e174882de1d64b1020b73ff64828c
Adam Scott <ascott1@gmail.com>
2019-08-31 08:23:23 -0400

    app shell solutions

---
+++ solutions/01-app-shell/Main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Screens from './screens';
+
+const Main = () => {
+  return <Screens />;
+};
+
+export default Main;
---
+++ solutions/01-app-shell/screens/note.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Note = () => {
+  return (
+    <View style={{ padding: 10 }}>
+      <Text>This is a note!</Text>
+    </View>
+  );
+};
+
+export default Note;
